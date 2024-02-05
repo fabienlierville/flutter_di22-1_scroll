d0b0bdc03e548dcb8f12cf054bf393339f1aad21
fabien <flierville@cesi.fr>
2024-02-05 10:17:36 +0100

    ScrollController

--- lib/pages/list_view_page.dart
+++ lib/pages/list_view_page.dart
@@ -47,25 +47,75 @@ class _ListViewPageState extends State<ListViewPage> {
     Activite(nom: "Bricolage", icone: Icons.build),
   ];
 
+  ScrollController scrollController = ScrollController();
+
+  initState(){
+    super.initState();
+    scrollController.addListener(infinityScroll);
+  }
+
+  infinityScroll(){
+    print("Position = ${scrollController.position.pixels}");
+    print("Taille Max = ${scrollController.position.maxScrollExtent}");
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("ListView"),),
-      body: ListView.builder(
-          itemCount: activites.length,
-          itemBuilder: (context, index){
-            Activite activite = activites[index];
-            return ListTile(
-              title:  Text("Activité"),
-              subtitle: Text(activite.nom),
-              trailing: Icon(Icons.arrow_forward_ios),
-              leading: Icon(activite.icone),
-              onTap: (){
-                print(activite.nom);
-                //Une navigation vers une nouvelle page
-              },
-            );
-          }
+      appBar: AppBar(
+        title: Text("ListView"),
+      ),
+      body: Scrollbar(
+        thickness: 10,
+        child: ListView.separated(
+          controller: scrollController,
+            itemCount: activites.length,
+            separatorBuilder: (context, index) {
+              if (index % 10 == 0 && index != 0) {
+                return Container(height: 100, color: Colors.red);
+              } else {
+                return Divider();
+              }
+            },
+            itemBuilder: (context, index) {
+              Activite activite = activites[index];
+              return Dismissible(
+                background: Container(
+                    color: Colors.red,
+                    child: Text(
+                      "Supprimer",
+                    )),
+                secondaryBackground: Container(
+                    color: Colors.green,
+                    child: Text(
+                      "Archiver",
+                      textAlign: TextAlign.right,
+                    )),
+                onDismissed: (direction) {
+                  setState(() {
+                    if (direction == DismissDirection.endToStart) {
+                      activites.removeAt(index);
+                      // Archiver le mail
+                    } else {
+                      activites.removeAt(index);
+                      // supprimer le mail
+                    }
+                  });
+                },
+                key: Key(activite.nom),
+                child: ListTile(
+                  title: Text("Activité"),
+                  subtitle: Text(activite.nom),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                  leading: Icon(activite.icone),
+                  onTap: () {
+                    print(activite.nom);
+                    //Une navigation vers une nouvelle page
+                  },
+                ),
+              );
+            }),
       ),
     );
   }
